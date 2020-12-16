20c1595c6d5a30df0893dc730ef4ba6e5b9dc6dc
Simon Forster <sforster@academytrainee.com>
2020-12-16 13:00:33 +0000

    Added create statements for the items & categories tables in the schema files

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -8,3 +8,18 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `email` VARCHAR(100) NULL DEFAULT NULL,
     PRIMARY KEY (`customer_id`)
 );
+CREATE TABLE IF NOT EXISTS `ims`.`categories` (
+    `category_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(40) NULL DEFAULT NULL,
+    PRIMARY KEY (`category_id`)
+);
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+    `item_id` INT(11) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(40) NULL DEFAULT NULL,
+    `value` FLOAT(20) NOT NULL DEFAULT 0,
+    `category_id` INT(11) NULL DEFAULT NULL,
+    `quanity` INT(11) NOT NULL DEFAULT 0,
+    PRIMARY KEY (`item_id`),
+    FOREIGN KEY (`category_id`) REFERENCES `ims`.`categories`(`category_id`)
+);
+
