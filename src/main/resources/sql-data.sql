fe3f49e1ee60e72694eb13c02272192532f3a9ab
Simon Forster <sforster@academytrainee.com>
2020-12-18 12:21:08 +0000

    Added sql test data

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,3 +1,7 @@
 INSERT INTO `ims`.`customers` (`first_name`, `surname`, `email`) VALUES ('Jordan', 'Harrison', 'jharrison@qa.com');
 INSERT INTO `ims`.`categories` (`category_id`, `name`) VALUES (1, 'General');
 INSERT INTO `ims`.`items` (`name`, `value`, `category_id`, `quantity`) VALUES ('Frying Pan', 15.99, 1, 46);
+INSERT INTO `ims`.`items` (`name`, `value`, `category_id`, `quantity`) VALUES ('Kettle', 24.99, 1, 57);
+INSERT INTO `ims`.`orders` (`customer_id`, `date`) VALUES (1, '2020-12-18');
+INSERT INTO `ims`.`order_items` (`order_id`, `item_id`, `quantity`) VALUES (1, 1, 2);
+INSERT INTO `ims`.`order_items` (`order_id`, `item_id`, `quantity`) VALUES (1, 2, 1);
--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS `ims`.`order_items` (
 	`order_items_id` INT(11) NOT NULL AUTO_INCREMENT,
 	`order_id` INT(11) NULL DEFAULT NULL,
 	`item_id` INT(11) NULL DEFAULT NULL,
-	`quantity` INT(7) NULL DEFAULT NULL,
+	`quantity` INT(11) NULL DEFAULT NULL,
 	PRIMARY KEY (`order_items_id`),
 	FOREIGN KEY (`order_id`) REFERENCES `ims`.`orders`(`order_id`),
 	FOREIGN KEY (`item_id`) REFERENCES `ims`.`items`(`item_id`)
